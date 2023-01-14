6090beedf764de676c27d3a9eb4613134be0ff58
Alexander <mypost@example.com>
2023-01-14 16:58:36 +0300

    1 com

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿// task 25
+
+int Func_(int x, int y)
+{
+    int val = x;
+    for (int i = 1; i < y; i++)
+    {
+        val = val * x;
+    }
+    return val;
+}
+
+Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите значение степени");
+int degree = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($" {num} в степени {degree} = {Func_(num, degree)}");
